5de77ee8a605ea7832232ed376112eb20726e296
mikej <jenkins957@gmail.com>
2020-12-06 19:03:30 +0000

    Day 6 solution.

---
+++ advent_of_code_2020/day06.py
@@ -0,0 +1,78 @@
+"""
+https://adventofcode.com/2020/day/6
+
+Note: Implemented for speed in terms of solving the solution, not for efficiency or scalability
+"""
+
+import unittest
+
+input_file = r'resources/day6_input.txt'
+
+
+def count_questions_answered_yes(groups):
+    count = 0
+
+    for group in groups:
+        yes_answers = set()
+        for line in group:
+            for c in line:
+                yes_answers.add(c)
+        count += len(yes_answers)
+
+    return count
+
+
+def count_questions_everyone_answered_yes(groups):
+
+    count = 0
+
+    for group in groups:
+        yes_answers = {}
+        for line in group:
+            for c in line:
+                if c in yes_answers:
+                    yes_answers[c] += 1
+                else:
+                    yes_answers[c] = 1
+
+        for value in yes_answers.values():
+            if value == len(group):
+                count += 1
+
+    return count
+
+
+def parse_input_data(filename):
+    groups = []
+    group = []
+
+    with open(filename) as file:
+        for line in file:
+            line = line.strip('\n')
+            if len(line) > 0:
+                group.append(line)
+            else:
+                groups.append(group)
+                group = []
+
+        groups.append(group)
+
+    return groups
+
+
+class TestDay6(unittest.TestCase):
+    def test_part_1(self):
+        result = count_questions_answered_yes(parse_input_data(input_file))
+
+        print('Part1:', result)
+        self.assertEqual(6930, result)
+
+    def test_part_2(self):
+        result = count_questions_everyone_answered_yes(parse_input_data(input_file))
+
+        print('Part2:', result)
+        self.assertEqual(3585, result)
+
+
+if __name__ == '__main__':
+    unittest.main()

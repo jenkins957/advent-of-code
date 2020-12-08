dca274ce18fa84fd7bd40647839909459ffd9724
mikej <jenkins957@gmail.com>
2020-12-08 15:06:14 +0000

    2020 - Day 8 solution. Consolidated parts 1 & 2 with generic execute_program function

--- advent_of_code_2020/day08_handheld_halting.py
+++ advent_of_code_2020/day08_handheld_halting.py
@@ -9,86 +9,7 @@ import unittest
 input_file = r'resources/day8_input.txt'
 
 
-def execute_program_part1(data):
-    accumulator = 0
-
-    instructions = parse_instructions(data)
-
-    # Keep track of all the unique instructions (via the pointer) to find first duplicate instruction executed
-    ins_pointers = set()
-
-    instruction_pointer = 0
-    while instruction_pointer < len(instructions):
-
-        # find the infinite loop and break out of the program - first duplicate instruction encountered
-        if instruction_pointer in ins_pointers:
-            break
-
-        ins_pointers.add(instruction_pointer)
-
-        ins = instructions[instruction_pointer]
-        op = ins[0]
-        val = ins[1]
-
-        if op == 'acc':
-            accumulator += val
-            instruction_pointer += 1
-        elif op == 'jmp':
-            instruction_pointer += val
-        else:
-            # nop
-            instruction_pointer += 1
-
-    return accumulator
-
-
-def identify_and_fix_infinite_loop(data):
-
-    # Try replacing nop with jmp then jmp wth nop to fix the infinte loop
-    result = __identify_and_fix_infinite_loop(data, 'jmp', 'nop')
-    if result[0] == -1:
-        result = __identify_and_fix_infinite_loop(data, 'nop', 'jmp')
-
-    return result[1]
-
-
-def __identify_and_fix_infinite_loop(data, source_ins, target_ins):
-
-    # result holds program exit code and accumulator result
-    result = (-1, 0)
-
-    line_index = 0
-
-    # brute force. Try replacing evey jmp with nop and retry program after each change, looking for a normal exit
-    for _ in range(len(data)):
-        # read instructions from file to ensure fresh
-        instructions = parse_instructions(data)
-
-        # modify next instruction
-        instructions = modify_program(instructions, line_index, source_ins, target_ins)
-        line_index = instructions[1]
-
-        result = execute_program_part2(instructions[0])
-        if result[0] == 0:
-            break
-
-    return result
-
-
-def modify_program(instructions, line_index, source, target):
-
-    for i in range(line_index, len(instructions)):
-        ins = instructions[i]
-
-        if ins[0] == source:
-            line_index = i
-            instructions[i] = (target, ins[1])
-            break
-
-    return instructions, line_index + 1
-
-
-def execute_program_part2(instructions):
+def execute_program(instructions):
     accumulator = 0
 
     # Process instructions
@@ -114,6 +35,53 @@ def execute_program_part2(instructions):
         instruction_pointer += 1
 
     return 0, accumulator
+
+
+def identify_and_fix_infinite_loop(data):
+
+    # Try replacing nop with jmp then jmp wth nop to fix the infinte loop
+    result = __identify_and_fix_infinite_loop(data, 'jmp', 'nop')
+    if result[0] == -1:
+        result = __identify_and_fix_infinite_loop(data, 'nop', 'jmp')
+
+    return result
+
+
+def __identify_and_fix_infinite_loop(data, source_ins, target_ins):
+
+    # result holds program exit code and accumulator result
+    result = (-1, 0)
+
+    line_index = 0
+
+    # brute force. Try replacing evey source_ins with the target_ins
+    # and retry program after each change, looking for a normal exit
+    for _ in range(len(data)):
+        # read instructions from file to ensure fresh
+        instructions = parse_instructions(data)
+
+        # modify next instruction
+        instructions = modify_program(instructions, line_index, source_ins, target_ins)
+        line_index = instructions[1]
+
+        result = execute_program(instructions[0])
+        if result[0] == 0:
+            break
+
+    return result
+
+
+def modify_program(instructions, line_index, source, target):
+
+    for i in range(line_index, len(instructions)):
+        ins = instructions[i]
+
+        if ins[0] == source:
+            line_index = i
+            instructions[i] = (target, ins[1])
+            break
+
+    return instructions, line_index + 1
 
 
 def parse_instructions(data):
@@ -142,16 +110,17 @@ def read_input_data(filename):
 
 class TestDay8HandheldHalting(unittest.TestCase):
     def test_part_1(self):
-        result = execute_program_part1(read_input_data(input_file))
+        instructions = parse_instructions(read_input_data(input_file))
+        result = execute_program(instructions)
 
-        print('Part1:', result)
-        self.assertEqual(1487, result)
+        print('Part1:', result[1])
+        self.assertEqual(1487, result[1])
 
     def test_part_2(self):
         result = identify_and_fix_infinite_loop(read_input_data(input_file))
 
-        print('Part2:', result)
-        self.assertEqual(1607, result)
+        print('Part2:', result[1])
+        self.assertEqual(1607, result[1])
 
 
 if __name__ == '__main__':

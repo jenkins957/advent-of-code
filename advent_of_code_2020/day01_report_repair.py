e1ff24f03bc0a2b180ba1618759d57ee64f142b1
mikej <jenkins957@gmail.com>
2021-01-04 10:57:04 +0000

    Optimised and improved Day 1 solution

--- advent_of_code_2020/day01_report_repair.py
+++ advent_of_code_2020/day01_report_repair.py
@@ -9,19 +9,41 @@ import unittest
 input_file = r'resources/day1_input.txt'
 
 
+def __two_sum(input_numbers, target):
+    numbers = set()
+
+    for n in input_numbers:
+        potential_match = target - n
+
+        if potential_match in numbers:
+            return potential_match, n
+
+        numbers.add(n)
+
+    return []
+
+
+def __three_sum(input_numbers, target):
+    for n in input_numbers:
+        potential_match = target - n
+
+        results = __two_sum(input_numbers, potential_match)
+        if len(results) == 2:
+            return results[0], results[1], n
+
+    return []
+
+
 def get_product_of_2_entries_that_add_to_target(expense_report, target=2020):
     """
     Find the product of 2 numbers in the list that add up to 2020
     :param expense_report: list of numbers
     :return: product of the 2 numbers
     """
+    numbers = __two_sum(expense_report, target)
 
-    # TODO : Edge case whereby same number could be used in both loops i.e if first element was 1010
-    for num1 in expense_report:
-        for num2 in expense_report:
-            if num1 + num2 == target:
-                print("Result", num1, num2, num1 * num2)
-                return num1 * num2
+    if len(numbers) == 2:
+        return numbers[0] * numbers[1]
     return -1
 
 
@@ -31,13 +53,10 @@ def get_product_of_3_entries_that_add_to_target(expense_report, target=2020):
     :param expense_report: list of numbers
     :return: product of the 3 numbers
     """
-    for num1 in expense_report:
-        for num2 in expense_report:
-            for num3 in expense_report:
-                if num1 + num2 + num3 == target:
-                    print("Result", num1, num2, num3, (num1 * num2 * num3))
-                    return num1 * num2 * num3
+    numbers = __three_sum(expense_report, target)
 
+    if len(numbers) == 3:
+        return numbers[0] * numbers[1] * numbers[2]
     return -1
 
 
